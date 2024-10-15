86a48d72b084841aab966f47cbb4346708968949
MellGriffin <missmelissagriffin@gmail.com>
2024-10-15 15:28:24 -0500

    init commit, plus added text to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// Home Page
+
+const App = () => {
+  return (
+    <>
+      <h1>Portfolio</h1>
+      <h2>Melissa Griffin</h2>
+      <h3>Junior Software Engineer</h3>
+      <p>
+      💾 Junior Software Engineer 
+    🎨 Artist, Digital Media & Drawing
+    💡 Creative Problem Solver
+    ⭐️ Enthusiastic Team Player 
+    📚 Bookworm
+    🔮 D&D Dungeon Master
+    🕹️ Gaming Enthusiast 
+      </p>
+      <div className="placeholder"></div>
+    </>
+  );
+};
+
+export default App;
