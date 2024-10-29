b598424eb557b1ad50f29a66f1010f91a61fd5e9
MellGriffin <missmelissagriffin@gmail.com>
2024-10-29 15:12:39 -0500

    Fixed nav bar to left

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,11 @@ const Home = () => {
         <div class="content">
         <h2>Home Page</h2>
         <p>Text goes here.</p>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
         </div>
       </>
     );
