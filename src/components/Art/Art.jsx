60440211c6983d0ff8cb80b30d2481f78abd6c9c
MellGriffin <missmelissagriffin@gmail.com>
2024-11-12 15:48:43 -0600

    Added google font Dangrek to nav bar

--- src/components/Art/Art.jsx
+++ src/components/Art/Art.jsx
@@ -1,9 +1,24 @@
+import './Art.css';
+
 const Art = () => {
 
     return (
         <>
+        <div className="artwork">
         <h2>Digital Illustrations</h2>
+        <div className="gallery">
         <img></img>
+        <img></img>
+        <img></img>
+        </div>
+        <p></p>
+        <h2>Sequential Art</h2>
+        <div className="gallery">
+        <img></img>
+        <img></img>
+        <img></img>
+        </div>
+        </div>
         </>
     )
 }
